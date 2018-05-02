e145792e32920ed82bd0abffbb9f59f7cff51f8e
Jose Torres <jtorr211@ford.com>
2018-05-01 20:33:12 -0500

    feat: Created Filtered Project

---
+++ Filtered/Filtered/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filtered
+//
+//  Created by tstone10 on 5/1/18.
+//  Copyright © 2018 DragonSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
