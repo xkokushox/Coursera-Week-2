fc1f7173a2c1e3e3846d14ce22de5b2159d954db
Jose Torres <jtorr211@ford.com>
2018-05-01 20:35:38 -0500

    chore: Add Filter Code

--- Filtered/Filtered/ViewController.swift
+++ Filtered/Filtered/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  Filtered
+//  Filterer
 //
-//  Created by tstone10 on 5/1/18.
+//  Created by tstone10 on 4/20/18.
 //  Copyright © 2018 DragonSoft. All rights reserved.
 //
 
@@ -10,9 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ivScarlett: UIImageView!
+    
+    @IBOutlet var bFilter: UIButton!
+    
+    var iFiltered: UIImage?
+    
+    @IBAction func onImageToggle(sender: UIButton) {
+        
+        if bFilter.selected{
+            ivScarlett.image = UIImage(named: "scarlett")!
+            bFilter.selected = false
+        }else{
+            ivScarlett.image = iFiltered
+            bFilter.selected = true
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "scarlett")!
+        
+        var rgbImage = RGBAImage(image: image)!
+        
+        let avgRed = 107
+        
+        for y in 0..<rgbImage.height{
+            for x in 0..<rgbImage.width{
+                let index = y * rgbImage.width + x
+                
+                var pixel = rgbImage.pixels[index]
+                
+                let redDelta = Int(pixel.red) - avgRed
+                
+                var modifier = 1 + 4 * (Double(y) / Double(rgbImage.height))
+                
+                if (Int(pixel.red)<avgRed){
+                    modifier = 1
+                }
+                
+                pixel.red = UInt8(max(min(255,
+                    Int(round(Double(avgRed)+modifier*Double(redDelta)))), 0))
+                
+                rgbImage.pixels[index] = pixel
+                
+            }
+        }
+        
+        iFiltered = rgbImage.toUIImage()
+        
     }
 
     override func didReceiveMemoryWarning() {
